612c103bf3afc008e82d8d923c4f47b2b7596b7d
Panagiotis Gkantis <pgkantis@novibet.com>
2024-09-30 14:28:30 +0300

    create sh scripts to create database on docker initialization

---
+++ dockerenv/run-initialization.sh
@@ -0,0 +1,9 @@
+# Wait to be sure that SQL Server came up
+echo "Initializing database..."
+sleep 25s
+
+# Run the setup script to create the DB and the schema in the DB
+# Note: make sure that your password matches what is in the Dockerfile
+/opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i create-database.sql
+
+echo "Database initialization completed!"
